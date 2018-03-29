c9468e3cb2dea59a9099351e174715f83062c08f
tem298 <tem298@nyu.edu>
2018-03-29 19:04:08 -0400

    fixed folder name

--- ChIPseq_Pipeline_v3/ChIPseq_PE_example.sh
+++ ChIPseq_Pipeline_v3/ChIPseq_PE_example.sh
@@ -22,12 +22,12 @@ FOLDER="$TAGC-$GENOME-B4W4"
 M2FILEPE="$TAGC-$GENOME-PE_B4W4"
 
 ji1=$(sbatch --export FLDR=$FLDR,FASTQIN=$CHIP,TAG=$TAGC,GENOME=$GENOME \
-    ~/Bowtie_PE_B4.sbatch | awk '{print $NF}')
+    ~/ChIPseq_Pipeline_v3/Bowtie_PE_B4.sbatch | awk '{print $NF}')
 
 ji2=$(sbatch --export FLDR=$FLDR,FASTQIN=$INPUT,TAG=$TAGI,GENOME=$GENOME \
-    ~/Bowtie_PE_B4.sbatch | awk '{print $NF}')
+    ~/ChIPseq_Pipeline_v3/Bowtie_PE_B4.sbatch | awk '{print $NF}')
 
 sbatch --dependency=afterok:$ji1:$ji2 --kill-on-invalid-dep=yes \
     --export FLDR=$FLDR,TREAT=$TREAT,CONTROL=$CONTROL,FOLDER=$FOLDER,M2FILE=$M2FILEPE,BDG="T",PEAK="BOTH",PE="True"  \
-    ~/MACS2_FE_W4.sbatch
+    ~/ChIPseq_Pipeline_v3/MACS2_FE_W4.sbatch
 
